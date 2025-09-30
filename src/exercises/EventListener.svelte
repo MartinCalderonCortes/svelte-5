3027acdd51d2b86b40e92e9b49284ff06b6ff8c2
Martin <mh.straus@hotmail.com>
2025-09-30 10:07:20 -0500

    Add exercise for event listener with the EventListener component

---
+++ src/exercises/EventListener.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+
+    let mouse = $state({ x: 0, y: 0})
+
+    function onmousemove(event: MouseEvent) {
+        mouse.x = event.clientX;
+        mouse.y = event.clientY;
+    }
+</script>
+
+<!-- svelte-ignore a11y_no_static_element_interactions -->
+<div class="mouse" {onmousemove}>
+    The mouse position is {mouse.x} x {mouse.y}
+</div>
