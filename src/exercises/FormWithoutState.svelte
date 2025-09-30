66cdf49a7d15d4f548fa5bbdbb6084b316638539
Martin <mh.straus@hotmail.com>
2025-09-30 10:08:10 -0500

    Add exercise for form withour state with the FormWitoutState component

---
+++ src/exercises/FormWithoutState.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    function onsubmit(event: SubmitEvent) {
+        event.preventDefault();
+        const data = new FormData(this)
+        const email = data.get('emailForm')
+        console.log(email)
+    }
+</script>
+
+<form {onsubmit}>
+    <input type="email" name="emailForm"/>
+    <button type="submit">Subscribe</button>
+</form>
