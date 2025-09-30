bc537b1a83eed60ebe559f97f26002b0c009b7e2
Martin <mh.straus@hotmail.com>
2025-09-30 10:05:59 -0500

    Add exercisen for data binding with the DataBinding component

---
+++ src/exercises/DataBinding.svelte
@@ -0,0 +1,29 @@
+ <!-- two-way data binding -->
+<script lang="ts">
+    let list = $state(["svelte", "react", "angular", "solid", "vue", "qwik"]);
+    let filteredList = $derived(list.filter((item) => item.includes(search)));
+    let search = $state("");
+
+    $inspect(search)
+</script>
+
+<!-- <input
+    type="search"
+    name="search"
+    value={search}
+    oninput={(event) => (search = event.currentTarget.value)}
+/> -->
+
+<input
+    type="search"
+    name="search"
+    bind:value={search}
+/>
+
+<ul>
+    {#each filteredList as item}
+        <li>{item}</li>
+    {:else}
+        <p>No items found</p>
+    {/each}
+</ul>
