a506d1e3cfae1202d1b0c4b4c3c8b3a01083de40
Martin <mh.straus@hotmail.com>
2025-09-30 10:09:12 -0500

    Test the different components added on the App.svelte root

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,16 @@
 <script>
 	import AsychonousData from "./exercises/AsychonousData.svelte";
+    import DataBinding from "./exercises/DataBinding.svelte";
     import EachBlock from "./exercises/EachBlock.svelte";
+    import EventListener from "./exercises/EventListener.svelte";
+    import FormWithoutState from "./exercises/FormWithoutState.svelte";
+    import FunctionBinding from "./exercises/FunctionBinding.svelte";
     import KeyBlock from "./exercises/KeyBlock.svelte";
     import LocalConstants from "./exercises/LocalConstants.svelte";
     import LocalConstants2 from "./exercises/LocalConstants2.svelte";
+    import ReadonlyBinding from "./exercises/ReadonlyBinding.svelte";
+    import Todo from "./todo/Todo.svelte";
 
 </script>
 
-<LocalConstants2/>
+<Todo/>
